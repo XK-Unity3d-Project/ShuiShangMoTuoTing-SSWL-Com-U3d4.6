460983db97769163dc05bb74a36bf52590234226
czq <czq@gmail.com>
2017-01-08 11:23:02 +0800

    先不要影响其它部分的代码

--- SetPanel.cs
+++ SetPanel.cs
@@ -31,7 +31,7 @@ public class SetPanel : MonoBehaviour
 	void Start () 
 	{
 		XkGameCtrl.IsLoadingLevel = false;
-		GameAudioVolume = ReadGameInfo.GetInstance().AudioVolume;
+		GameAudioVolume = ReadGameInfo.GetInstance().ReadGameAudioVolume();
 		GameAudioVolumeLB.text = GameAudioVolume.ToString();
 
 		IsOpenSetPanel = true;
@@ -453,15 +453,15 @@ public class SetPanel : MonoBehaviour
 						GameAudioVolume = 0;
 					}
 					GameAudioVolumeLB.text = GameAudioVolume.ToString();
-					ReadGameInfo.GetInstance().AudioVolume = GameAudioVolume;
+					ReadGameInfo.GetInstance().WriteGameAudioVolume(GameAudioVolume);
 					break;
 				}
 			case 12:
 				{
 					GameAudioVolume = 7;
 					GameAudioVolumeLB.text = GameAudioVolume.ToString();
-                    ReadGameInfo.GetInstance().AudioVolume = GameAudioVolume;
-                    break;
+					ReadGameInfo.GetInstance().WriteGameAudioVolume(GameAudioVolume);
+					break;
 				}
             case 13:
                 {
